7aa04b927c9d0932338aa62b969064e41cfb6990
Snezhana Makhova <makhova-s94@mail.ru>
2023-06-21 23:48:13 +0300

    corrected Item calculate_total_price, init and apply_discount added tests to these three

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,27 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def item_fixture():
+    """
+    Фикстура для тестов
+    """
+    return Item("Смартфон", 10000, 20)
+
+def test_item_init(item_fixture):
+    """
+    Проверяем, что в есть такое название товара
+    """
+    assert item_fixture.name == "Смартфон"
+
+def test_calculate_total_price(item_fixture):
+    """
+    Проверяем, что сумма товаров равна заданному числу
+    """
+    assert item_fixture.price * item_fixture.quantity == 200000
+
+def test_apply_discount(item_fixture):
+    """
+    Проверяем, что цена со скидкой равна заданному числу
+    """
+    assert item_fixture.price * 0.8 == 8000.0
